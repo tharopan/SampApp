11884d1226443ca71dea498185dc7745ce79ee20
tharopan <tharopan@hotmail.com>
2015-01-04 01:43:51 +0530

    Modified
    Modified

--- SampAppDAL/Context/SampAppContext.cs
+++ SampAppDAL/Context/SampAppContext.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using SampAppDAL.Dtos;
+
 
 namespace SampAppDAL.Context
 {
-    class SampAppContext
+    public class SampAppContext : DbContext
     {
+        public SampAppContext() : base()
+        {
+                
+        }
+
+        DbSet<StudentDto> Students { get; set; }
     }
 }
